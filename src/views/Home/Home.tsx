6d350abe05d94ae27a02d6e5b52e76a882f91e62
Darius Foodeei <darius.foodei@gmail.com>
2021-06-08 18:05:40 +0200

    genswap components

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -91,7 +91,7 @@ const Home: React.FC = () => {
     <Page>
       <Hero>
         <Heading as="h1" scale="xl" mb="24px" color="secondary">
-          {t('PancakeSwap')}
+          {t('Genswap')}
         </Heading>
         <Text>{t('The #1 AMM and yield farm on Binance Smart Chain.')}</Text>
       </Hero>
